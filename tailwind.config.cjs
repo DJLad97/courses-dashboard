0b6a9321ebc0e5b124f3598647c0cedb0e55a484
DJLad97 <dj-scott1@hotmail.co.uk>
2023-02-27 20:14:25 +0000

    Setup tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter"]
+            }
+        },
+		colors: {
+			"neutral-900": "#292C3E",
+			"neutral-600": "#292C3E",
+			"neutral-100": "#F0F1F5",
+			"primary-800": "#2C1674",
+			"primary-700": "#3D1FA2",
+			"primary-200": "#DDD5FA",
+		}
+    },
+	plugins: [],
+}
