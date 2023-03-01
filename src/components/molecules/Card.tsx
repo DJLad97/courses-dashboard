79ced31efe830bb0a53c2108c396623b4c46f204
DJLad97 <dj-scott1@hotmail.co.uk>
2023-03-01 19:37:17 +0000

    Refactored card

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -10,12 +10,12 @@ type Props = {
 
 function Card ({ icon, title, description }: Props) {
     return (
-        <div className="flex flex-col bg-white rounded shadow-lg pl-4 mr-10">
+        <div className="flex flex-col bg-white rounded shadow-lg px-4 mr-10">
             <div className="mt-7">
                 <Icon src={icon} />
             </div>
             <h2 className="text-xl font-semibold mt-4">{ title }</h2>
-            <p className="mt-2 mb-6 ax-w-[30ch]">
+            <p className="mt-2 mb-6 min-w-[258px] max-w-[258px]">
                 { description }
             </p>
             <div className="flex mt-auto mb-6">
